4aa037256ce72836ca8e9c357cc504066e6f95c7
armaancha <145879027+armaancha@users.noreply.github.com>
2023-11-01 20:36:13 -0700

    Update game.js
    Added settings, check win, levels, and made some updates.

--- game.js
+++ game.js
@@ -3,23 +3,45 @@
 const prompt = require("prompt-sync")({ sigint: true });
 
 class WordManager {
-  easyWords = ["cat", "dog", "ball", "park"]
+  easyWords = ["cct", "ddg", "ball", "aark"]
   hardWords = ["elephant", "telephone", "computer", "aeroplane", "universe"]
   index = 0;
   
   getWord = (level) => {
-    if(level==="easy") {
+    if(level===1) {
       this.index = Math.floor(Math.random() * this.easyWords.length);
       return this.easyWords[this.index];
     }
 
-    if(level==="hard") {
+    if(level===2) {
       this.index = Math.floor(Math.random() * this.hardWords.length);
       return this.hardWords[this.index];
     }
   }
 }
 
+class SettingsManager 
+  settings = {
+    level: 1,
+    guessCount: 10
+  }
+
+  askSettings = () => {
+    this.settings.level = parseInt(prompt("What level of difficulty? 1 for easy, 2 for hard, 3 for impossible."))
+
+    if (this.settings.level===1) {
+      this.settings.guessCount = 10;
+    }
+    else if (this.settings.level===2) {
+      this.settings.guessCount = 5;
+    }
+    else if (this.settings.level===3) {
+      this.settings.guessCount = 3;
+    }
+
+    return this.settings;
+  }
+}
 
 class HangmanGame {
   word = "";
@@ -33,12 +55,19 @@ class HangmanGame {
     this.word = "";
     this.playerWord = "";
   } 
+
+  checkWin = () => {
+    return this.playerWord.includes("_") ? false : true;
+  }
   
   play = () => {
     this.initialize();
+
+    let sm = new SettingsManager();
+    let settings = sm.askSettings();
     
     let wm = new WordManager();
-    this.word = wm.getWord("easy");
+    this.word = wm.getWord(settings.level).toUpperCase();
     for(let i=0;i<this.word.length;i++) {
       this.playerWord = this.playerWord + "_";
     }
@@ -48,6 +77,7 @@ class HangmanGame {
     let solved = false;
     while(!solved) {
       let letter = prompt("What is your guess? ")
+      letter = letter.toUpperCase();
       if(letter.length!=1 || 
          !this.isLetter(letter) || 
          guesses.includes(letter) || 
@@ -55,15 +85,22 @@ class HangmanGame {
         console.log("Invalid guess.");
         continue;
       }
+      
       for(let i=0;i<this.word.length;i++) {
         if(this.word[i] === letter) {
           let temp = this.playerWord.split("");
           temp[i] = letter;
-          this.playerWord = temp.toString();
-          console.log("Your guess was right!");
-          console.log(this.playerWord);
+          this.playerWord = temp.join("");
         }
       }
+      guesses.push(letter);
+      if(this.checkWin()) {
+        console.log("\nYou won!")
+        console.log(this.playerWord + "\n");
+        return;
+      }
+      console.log("\nYour guess was right!");
+      console.log(this.playerWord + "\n");
     }
   }
 }
