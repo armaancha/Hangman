1f2b728218ce624334d4734b3206a1c0005865b6
armaancha <145879027+armaancha@users.noreply.github.com>
2023-11-04 22:39:50 -0700

    Update game.js

--- game.js
+++ game.js
@@ -1,7 +1,6 @@
 // Code for Hangman game. Runs in JavaScript Nodejs.
 
-const {HangmanGame} = require("./hangmanGame.js")
-
+const { HangmanGame } = require("./hangmanGame.js")
 
 let game = new HangmanGame();
 game.play();
